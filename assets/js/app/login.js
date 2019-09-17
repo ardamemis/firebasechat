98ac2129213a3e331b56d84842863a646fc23a50
ardamemis <48928106+ardamemis@users.noreply.github.com>
2019-09-17 22:02:03 +0300

    Add files via upload

---
+++ assets/js/app/login.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyD8fzmX1SQK261tilmjZoqapUADsib_2UQ",
+        authDomain: "ninthapp-aae3d.firebaseapp.com",
+        databaseURL: "https://ninthapp-aae3d.firebaseio.com",
+        projectId: "ninthapp-aae3d",
+        storageBucket: "ninthapp-aae3d.appspot.com",
+        messagingSenderId: "370611217911",
+        appId: "1:370611217911:web:fff0b912fbd5f121f83af2"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+
+    $("#loginBtn").click(function(){
+
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        firebase.auth().signInWithUsernameAndPassword(email, password)
+            .then(function(){
+                window.location.href = "index.html";
+            }).catch(function(error){
+                alert(error.message);
+        })
+
+
+    })
+
+})
---
+++ assets/js/app/register.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyD8fzmX1SQK261tilmjZoqapUADsib_2UQ",
+        authDomain: "ninthapp-aae3d.firebaseapp.com",
+        databaseURL: "https://ninthapp-aae3d.firebaseio.com",
+        projectId: "ninthapp-aae3d",
+        storageBucket: "ninthapp-aae3d.appspot.com",
+        messagingSenderId: "370611217911",
+        appId: "1:370611217911:web:fff0b912fbd5f121f83af2"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+
+
+    $("#registerBtn").click(function(){
+
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        firebase.auth().createUserWithUsernameAndPassword(email, password)
+            .then(function(){
+
+                firebase.auth().signInWithEmailAndPassword(email, password)
+                    .then(function(){
+                        window.location.href = "index.html";
+                    })
+
+            }).catch(function(error){
+                alert(error.message);
+        })
+
+    })
+
+
+})
